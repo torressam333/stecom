989e6c7a8c62fd83a0ae45dbcfe3142861521284
Samuel <torressam333@gmail.com>
2019-04-04 14:12:16 -0700

    Created base controller and index controller

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -1,14 +1,19 @@
 <?php
+use App\Controllers\IndexController;
+
 
 //Create instance of alto router class
 $router = new AltoRouter;
 
-$router->map('GET', '/about', '', 'about us');
+$router->map('GET', '/', '', 'Home');
 
 $match = $router->match();
 
 if($match){
-    echo 'About us page ';
+    require_once __DIR__. '/../controllers/BaseController.php';
+    require_once __DIR__. '/../controllers/IndexController.php';
+    $index = new IndexController();
+    $index->show();
 }else{
     header($_SERVER['SERVER_PROTOCOL']. '404 Not Found');
     echo "Page not found";
